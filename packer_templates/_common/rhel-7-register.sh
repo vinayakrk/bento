140522d1ca16652b76b8fc7ffe3b5fa99e8752c2
vrk <vrk@zen.vinayak.net>
2021-02-22 18:42:07 -0500

    Merged version specific register commands

--- packer_templates/_common/rhel-7-register.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -eux
-
-. ~/.rhel_secrets
-
-subscription-manager register --username="$RHEL_USERNAME" --password="$RHEL_PASSWORD" --auto-attach
-subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"
-yum update -yyy
-reboot
--- packer_templates/_common/rhel-8-register.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash -eux
-
-. ~/.rhel_secrets
-
-subscription-manager register --username="$RHEL_USERNAME" --password="$RHEL_PASSWORD" --auto-attach
-subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
-dnf update -yyy
-dnf upgrade -yyy
-reboot
